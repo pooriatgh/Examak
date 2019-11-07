99987386967c34a85344021b4e1e4d0a862bfef4
Pooria Taghizadeh <Pooria Taghizadeh@DESKTOP-59NL040>
2019-11-07 14:49:35 +0330

    accunt js is done

--- Exam/Controllers/AccuntController.cs
+++ Exam/Controllers/AccuntController.cs
@@ -11,19 +11,26 @@ namespace Exam.Controllers
        
         public JsonResult Login(string mobile,string password)
         {
-            return Json(null);
+            return Json("ok");
         }
         public JsonResult Signup(string mobile,string password)
         {
+            return Json(null);
+        }
+        public JsonResult ForgotPassword(string mobile)
+        {
+            return Json(null);
+        }
+        public JsonResult ConfirmNewPassword(string mobile,string code ,string newPassword){
             return Json(null);
         }
         public JsonResult CompleteSignup(string mobile,string code)
         {
             if (true)//code ok and time doesnt expired
             {
-                RedirectToAction("Index","Profile");
+                return Json("ok");
             }
-            return Json(null);
+            return Json("nok");
         }
     }
 }
---
+++ Exam/Scripts/Costume/Accunt.js
@@ -0,0 +1,73 @@
+﻿function Login() {
+    var mobile = $("#mobile").val()
+    var password = $("#password").val()
+    var data = { 'mobile': mobile, 'password': password }
+    $.post("/accunt/login",
+        data, function (result) {
+            if (result == "ok") {
+                window.location.replace("/home/about");
+            }
+            else {
+                message = result
+            }
+    });
+}
+
+function Signup() {
+    var mobile = $("#mobile").val()
+    var password = $("#password").val()
+    var confirmPassword = $("#confirmPassword").val()
+    var data = { 'mobile': mobile, 'password': password }
+    if (password == confirmPassword) {
+        $.post("/accunt/signup",
+            data, function (result) {
+                alert(result);
+            });
+    }
+    else{
+        $('#password').css('border-color', 'red');
+        $('#confirmPassword').css('border-color', 'red');
+    }
+   
+}
+
+function CompleteSignup() {
+    var mobile = $("#mobile").val()
+    var code = $("#code").val()
+    var data = { 'mobile': mobile, 'code': code }
+    $.post("/accunt/CompleteSignup",
+        data, function (result) {
+            if (result == "ok") {
+                window.location.replace("/home/about");
+            }
+        });
+}
+
+function ForgotPassword() {
+    var mobile = $("#mobile").val()
+    var data = { 'mobile': mobile}
+    $.post("/accunt/ForgotPassword",
+        data, function (result) {
+            if (result == "ok") {
+                window.location.replace("/home/about");
+            }
+        });
+}
+
+function ConfirmNewPassword() {
+    var mobile = $("#mobile").val()
+    var password = $("#password").val()
+    var code = $("#code").val()
+    var confirmPassword = $("#confirmPassword").val()
+    var data = { 'mobile': mobile, 'newPassword': password, 'code': code }
+    if (password == confirmPassword) {
+        $.post("/accunt/ConfirmNewPassword",
+            data, function (result) {
+                alert(result);
+            });
+    }
+    else {
+        $('#password').css('border-color', 'red');
+        $('#confirmPassword').css('border-color', 'red');
+    }
+}
