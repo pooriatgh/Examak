5f85a14cd5a502b182d8c0e7a405c8b871f6c651
Pooria Taghizadeh <Pooria Taghizadeh@DESKTOP-59NL040>
2019-11-09 16:05:36 +0330

    payment done

--- Exam/Controllers/PaymentController.cs
+++ Exam/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using Exam.Models;
+﻿using Exam.Data;
+using Exam.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Web.Mvc;
 
 namespace Exam.Controllers
 {
+    [AuthorizationEN]
     public class PaymentController : Controller
     {
         public void BeginPayment(Guid packageId)
@@ -15,16 +17,35 @@ namespace Exam.Controllers
             try
             {
                 //get packageid and set amount
-
+                var db = new ENDEntities();
+                var user = (Tbl_User)Session["UserSession"];
+                decimal amount = 1000;
+                var newPayment = new Tbl_Payment
+                {
+                    PK_PaymentId = Guid.NewGuid(),
+                    Amount = amount,
+                    CreateDatetime = DateTime.UtcNow,
+                    FK_UserId = user.PK_UserId,
+                    IsVerified = false,
+                    RecieveError = String.Empty,
+                    SendError = String.Empty
+                };
+                db.Tbl_Payment.Add(newPayment);
+                db.SaveChanges();
                 PaymentHandler ob = new PaymentHandler();
-                string result = ob.pay("1000");
+                string result = ob.pay(amount.ToString());
                 JsonParameters Parmeters = JsonConvert.DeserializeObject<JsonParameters>(result);
                 if (Parmeters.status == 1)
                 {
+                    newPayment.TransactionId = Parmeters.transId;
+                    newPayment.HashTransaction = HashHandler.Hash(Parmeters.transId);
+                    db.SaveChanges();
                     Response.Redirect("https://pay.ir/payment/gateway/" + Parmeters.transId);
                 }
                 else
                 {
+                    newPayment.RecieveError = "کدخطا : " + Parmeters.errorCode + "<br />" + "پیغام خطا : " + Parmeters.errorMessage;
+                    db.SaveChanges();
                     ViewBag["Error"] = "کدخطا : " + Parmeters.errorCode + "<br />" + "پیغام خطا : " + Parmeters.errorMessage;
                 }
             }
@@ -37,20 +58,58 @@ namespace Exam.Controllers
         {
             try
             {
+              
                 if (!string.IsNullOrEmpty(Request.Form["transId"]))
                 {
                     PaymentHandler ob = new PaymentHandler();
-                    string result = ob.verify(Request.Form["transId"].ToString());
+                    string transId = Request.Form["transId"].ToString();
+                    var db = new ENDEntities();
+                    var timenow = DateTime.UtcNow;
+                    var user = (Tbl_User)Session["UserSession"];
+                    var payment = db.Tbl_Payment.Where(p => p.FK_UserId == user.PK_UserId &&
+                    p.TransactionId == transId && p.IsVerified == false).FirstOrDefault();
+                    string result = ob.verify(transId);
                     JsonParameters Parmeters = JsonConvert.DeserializeObject<JsonParameters>(result);
-
                     if (Parmeters.status == 1)
                     {
+                       
+                        if (payment!=null)
+                        {
+                            payment.IsVerified = true;
+                            if (payment.Amount == 500000)
+                            {
+                                payment.Tbl_User.WritingAvailable += 1;
+                                payment.Tbl_User.SpeakingAvailable += 1;
+                            }else if (payment.Amount == 1000000)
+                            {
+                                payment.Tbl_User.WritingAvailable += 3;
+                                payment.Tbl_User.SpeakingAvailable += 2;
+                            }
+                            else if (payment.Amount == 2000000)
+                            {
+                                payment.Tbl_User.WritingAvailable += 6;
+                                payment.Tbl_User.SpeakingAvailable += 6;
+                            }
+                            else if (payment.Amount == 5000000)
+                            {
+                                payment.Tbl_User.WritingAvailable += 16;
+                                payment.Tbl_User.SpeakingAvailable += 16;
+                            }
+                            else if (payment.Amount == 10000000)
+                            {
+                                payment.Tbl_User.WritingAvailable += 40;
+                                payment.Tbl_User.SpeakingAvailable += 40;
+                            }
+                            db.SaveChanges();
+                        }
                         ViewBag["PaymentRresult"] += Request.Form["transId"].ToString();
                         ViewBag["PaymentRresult"] += Parmeters.amount.ToString();
                         ViewBag["PaymentRresult"] = "پرداخت با موفقیت انجام شد.";
                     }
                     else
                     {
+                        payment.RecieveError = "کدخطا : " + Parmeters.errorCode + "<br />" + "پیغام خطا : " + Parmeters.errorMessage;
+                        db.SaveChanges();
                         ViewBag["Error"] = true;
                         ViewBag["Error"] = "کدخطا : " + Parmeters.errorCode + "<br />" + "پیغام خطا : " + Parmeters.errorMessage;
                     }
