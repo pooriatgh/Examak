1d189fc723dd8ea0fd7d5c4704ab3a019affe256
Pooria Taghizadeh <Pooria Taghizadeh@DESKTOP-59NL040>
2019-11-09 15:07:10 +0330

    accunt is completed

--- Exam/Controllers/AccuntController.cs
+++ Exam/Controllers/AccuntController.cs
@@ -10,28 +10,56 @@ namespace Exam.Controllers
 {
     public class AccuntController : Controller
     {
-       
-        public JsonResult Login(string mobile,string password)
+
+        public JsonResult Login(string mobile, string password)
         {
+            var db = new ENDEntities();
             var hashPassword = HashHandler.Hash(password);
-            if (true)// exist in database and active
+            var user = db.Tbl_User.Where(p => p.Mobile == mobile && p.Password == hashPassword).FirstOrDefault();
+            if (user != null)// exist in database and active
             {
-                Session["UserSession"] = new Tbl_User { Mobile = mobile, Password = String.Empty };
+                if (user.IsActive)
+                {
+                    Session["UserSession"] = user;
+                    return Json("ok");
+                }
+                else
+                {
+                    return Json("notActive");
+                }
             }
-            return Json("ok");
+            else
+            {
+                return Json("userOrPasswordIncorrect");
+            }
         }
-        public JsonResult Signup(string mobile,string password)
+        public JsonResult Signup(string mobile, string password)
         {
-            if (false)// mobile does not exist with 0 or 98 or +98 or any
+            var db = new ENDEntities();
+            mobile = mobile.Replace("+", "").Replace("98", "");
+            if (mobile.ElementAt(0) != 0)
+            {
+                mobile = "0" + mobile;
+            }
+            var userExist = db.Tbl_User.Where(p => p.Mobile == mobile).FirstOrDefault();
+            if (userExist == null)// mobile does not exist with 0 or 98 or +98 or any
             {
                 var hashPassword = HashHandler.Hash(password);
                 var newUser = new Tbl_User
                 {
                     Mobile = mobile,
                     Password = hashPassword,
-                    MobileIsActive = false,
                     ResetPassCode = String.Empty,
-                    SignupCode = Guid.NewGuid().ToString().Substring(0, 4)
+                    SignupCode = Guid.NewGuid().ToString().Substring(0, 4),
+                    PK_UserId = Guid.NewGuid(),
+                    IsActive = false,
+                    Name = "Guest",
+                    CreateDatetime = DateTime.UtcNow,
+                    SignupCodeExpireTime = DateTime.UtcNow.AddDays(1),
+                    ResetCodeExpireTime = DateTime.UtcNow,
+                    SpeakingAvailable = 1,
+                    WritingAvailable = 1
+
                 };
                 SMSHandler.SendMessage(mobile, "کد سایت آزمونک" + " " + newUser.SignupCode + " ");
             }
@@ -39,16 +67,50 @@ namespace Exam.Controllers
         }
         public JsonResult ForgotPassword(string mobile)
         {
-            SMSHandler.SendMessage(mobile,Guid.NewGuid().ToString().Substring(0, 4));
-            return Json(null);
-        }
-        public JsonResult ConfirmNewPassword(string mobile,string code ,string newPassword){
-            return Json(null);
-        }
-        public JsonResult CompleteSignup(string mobile,string code)
-        {
-            if (true)//code ok and time doesnt expired
+            var db = new ENDEntities();
+            var userExist = db.Tbl_User.Where(p => p.Mobile == mobile).FirstOrDefault();
+            if (userExist != null)
             {
+                var code = Guid.NewGuid().ToString().Substring(0, 4);
+                userExist.ResetPassCode = code;
+                userExist.ResetCodeExpireTime = DateTime.UtcNow.AddDays(1);
+                db.SaveChanges();
+                SMSHandler.SendMessage(mobile, code + " کد تغییر رمز ");
+            }
+            return Json(null);
+        }
+        public JsonResult ConfirmNewPassword(string mobile, string code, string newPassword)
+        {
+            if (code != String.Empty)
+            {
+                var db = new ENDEntities();
+                var timeNow = DateTime.UtcNow;
+                var userExist = db.Tbl_User.Where(p => p.Mobile == mobile &&
+                p.ResetPassCode == code && p.ResetCodeExpireTime < timeNow).FirstOrDefault();
+                if (userExist != null)
+                {
+                    userExist.Password = HashHandler.Hash(newPassword);
+                    userExist.ResetPassCode = String.Empty;
+                    db.SaveChanges();
+                    return Json("PasswordChangeOK");
+                }
+                else
+                {
+                    return Json("CodeOrTimeIsWrong");
+                }
+            }
+            return Json("CodeEmpty");
+        }
+        public JsonResult CompleteSignup(string mobile, string code)
+        {
+            var db = new ENDEntities();
+            var timeNow = DateTime.UtcNow;
+            var userExist = db.Tbl_User.Where(p => p.Mobile == mobile &&
+            p.SignupCode == code && p.SignupCodeExpireTime < timeNow).FirstOrDefault();
+            if (code!=String.Empty && userExist!=null )//code ok and time doesnt expired
+            {
+                userExist.IsActive = true;
+                db.SaveChanges();
                 return Json("ok");
             }
             return Json("nok");
